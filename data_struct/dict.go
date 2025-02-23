117cd5f844cadd7fea3cd77a70047a9d8efc764c
POABOB <zxc752166@yahoo.com.tw>
2025-02-23 11:04:58 +0800

    feat: define the interface of Dict

---
+++ data_struct/dict.go
@@ -0,0 +1,18 @@
+package data_struct
+
+// Consumer is a callback function, if return true, it will continue to iterate
+type Consumer func(key string, value interface{}) bool
+
+type Dict interface {
+	Get(key string) (value interface{}, exists bool)
+	Length() int
+	Set(key string, value interface{}) (result int)
+	SetIfAbsent(key string, value interface{}) (result int)
+	SetIfExists(key string, value interface{}) (result int)
+	Delete(key string) (result int)
+	ForEach(consumer Consumer)
+	Keys() []string
+	RandomKeys(limit int) []string
+	RandomDistinctKeys(limit int) []string
+	clear()
+}
